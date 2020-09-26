72b60f09a63499fedabbec3c617eb1dc2e2ba057
可可鲁 5 <kekelu5@kekeludeMacBook-Pro.local>
2020-09-26 22:53:19 +0800

    completed proj1gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,60 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+
+    private static String message = "";
+    private static int callTimes = 50;
+
+
+    private void randomAdd(double r, Integer i,
+                           StudentArrayDeque<Integer> test, ArrayDequeSolution<Integer> right) {
+        if (r < 0.5) {
+            test.addFirst(i);
+            right.addFirst(i);
+            message += "\naddFirst(" + i + ") ";
+        } else {
+            test.addLast(i);
+            right.addLast(i);
+            message += "\naddLast(" + i + ") ";
+        }
+    }
+
+    private void randomRemove(double r,
+                              StudentArrayDeque<Integer> test, ArrayDequeSolution<Integer> right) {
+        Integer expected;
+        Integer actual;
+        if (r < 0.5) {
+            expected = test.removeFirst();
+            actual = right.removeFirst();
+            message += "\nremoveFirst() ";
+        } else {
+            expected = test.removeLast();
+            actual = right.removeLast();
+            message += "\nremoveLast() ";
+        }
+        assertEquals(message, expected, actual);
+    }
+
+    @Test
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> test1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> right1 = new ArrayDequeSolution<>();
+
+        for (Integer i = 1; i < callTimes; i += 1) {
+            if (test1.isEmpty()) {
+                double random = StdRandom.uniform();
+                randomAdd(random, i, test1, right1);
+            } else {
+                double random1 = StdRandom.uniform();
+                double random2 = StdRandom.uniform();
+                if (random1 < 0.5) {
+                    randomAdd(random2, i, test1, right1);
+                } else {
+                    randomRemove(random2, test1, right1);
+                }
+            }
+        }
+    }
+
+}
