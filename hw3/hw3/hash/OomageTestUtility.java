e9f96ce0257af8ca07a4b89e51d0b2031e7c1b8d
keke1u <zhongchk3@mail2.sysu.edu.cn>
2020-11-06 23:26:34 +0800

    first commit hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,10 +1,12 @@
 package hw3.hash;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OomageTestUtility {
     public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
-        /* TODO:
+        /*
          * Write a utility function that returns true if the given oomages
          * have hashCodes that would distribute them fairly evenly across
          * M buckets. To do this, convert each oomage's hashcode in the
@@ -12,6 +14,24 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        Map<Integer, Integer> bucketNumMap = new HashMap<>();
+        int N = oomages.size();
+        for (Oomage o : oomages) {
+            int bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            if (bucketNumMap.containsKey(bucketNum)) {
+                int bucketNumTimes = bucketNumMap.get(bucketNum);
+                bucketNumMap.put(bucketNum, bucketNumTimes + 1); // the value of existing key will be replaced
+            } else {
+                bucketNumMap.put(bucketNum, 1);
+            }
+        }
+
+        for (Integer bucket : bucketNumMap.keySet()) { // .keySet() to be iterable
+            int num = bucketNumMap.get(bucket);
+            if (num < N / 50 || num > N / 2.5) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -33,18 +33,27 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
-    /* TODO: Create a list of Complex Oomages called deadlyList
+    /* Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
+
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
         // Your code here.
+        ArrayList<Integer> params = new ArrayList<>();
+        for (int i = 0; i < 10; i += 1) {
+            params.add(255);
+        }
+
+        for (int i = 0; i < 200; i += 1) {
+            ComplexOomage coo = new ComplexOomage(params);
+            deadlyList.add(coo);
+        }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
